55173bf0bb79d1962f0b1b70d63d627e230016f4
Francisco Guillén <83434031+Francisco-Guillen@users.noreply.github.com>
2024-03-23 22:48:13 +0000

    Add files via upload

---
+++ K8055/K8055_TESTE/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace K8055_TESTE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+         #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Luzes = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ligar Carro";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(580, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Desligar Carro";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Luzes
+            // 
+            this.Luzes.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.Luzes.DisplayMember = "Luzes Desligadas";
+            this.Luzes.FormattingEnabled = true;
+            this.Luzes.Items.AddRange(new object[] {
+            "Luzes Desligadas",
+            "Mínimos",
+            "Médios",
+            "Máximos"});
+            this.Luzes.Location = new System.Drawing.Point(9, 197);
+            this.Luzes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Luzes.Name = "Luzes";
+            this.Luzes.Size = new System.Drawing.Size(92, 21);
+            this.Luzes.TabIndex = 4;
+            this.Luzes.ValueMember = "Luzes Desligadas";
+            this.Luzes.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 221);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(115, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Luzes de Nevoeiro";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(9, 243);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(52, 17);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "Pisca";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::K8055_TESTE.Properties.Resources.carro;
+            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(714, 444);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(316, 324);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(89, 17);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "Marcha Atrás";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(9, 266);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(60, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Travão";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 360);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Luzes);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox Luzes;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ K8055/K8055_TESTE/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+namespace K8055_TESTE
+{
+    public partial class Form1 : Form
+    {
+        Thread t;
+        public int aux = 0;
+        public int aux2 = 0;
+        public int ligado = -1;
+        public Boolean board_open = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Luzes.Text = "Luzes Desligadas";
+        }
+
+        public static class K8055
+        {
+            [DllImport("K8055D.dll")]
+            public static extern int OpenDevice(int CardAdress);
+
+            [DllImport("K8055D.dll")]
+            public static extern void CloseDevice();
+
+            [DllImport("K8055D.dll")]
+            public static extern int ReadAnalogChannel(int Channel);
+
+            [DllImport("K8055D.dll")]
+            public static extern void ReadAllAnalog(ref int Data1, ref int Data2);
+
+            [DllImport("K8055D.dll")]
+            public static extern void ClearAnalogChannel(int Channel);
+
+            [DllImport("K8055D.dll")]
+            public static extern void ClearAllAnalog();
+
+            [DllImport("K8055D.dll")]
+            public static extern void OutputAnalogChannel(int Channel, int Data);
+
+            [DllImport("K8055D.dll")]
+            public static extern void OutputAllAnalog(int Data1, int Data2);
+
+            [DllImport("K8055D.dll")]
+            public static extern void SetAnalogChannel(int Channel);
+
+            [DllImport("K8055D.dll")]
+            public static extern void SetAllAnalog();
+
+            [DllImport("K8055D.dll")]
+            public static extern void ClearAllDigital();
+
+            [DllImport("K8055D.dll")]
+            public static extern void ClearDigitalChannel(int Channel);
+
+            [DllImport("K8055D.dll")]
+            public static extern void SetAllDigital();
+
+            [DllImport("K8055D.dll")]
+            public static extern void SetDigitalChannel(int Channel);
+
+            [DllImport("K8055D.dll")]
+            public static extern void WriteAllDigital(int Data);
+
+            [DllImport("K8055D.dll")]
+            public static extern bool ReadDigitalChannel(int Channel);
+
+            [DllImport("K8055D.dll")]
+            public static extern int ReadAllDigital();
+
+            [DllImport("K8055D.dll")]
+            public static extern int ReadCounter(int CounterNr);
+
+            [DllImport("K8055D.dll")]
+            public static extern void ResetCounter(int CounterNr);
+
+            [DllImport("K8055D.dll")]
+            public static extern void SetCounterDebounceTime(int CounterNr, int DebounceTime);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            K8055.OpenDevice(0); //Open communication with K8055 that has the device address 0
+            
+            Console.WriteLine("Board Iniciada!");
+            //THREAD FOR READ THE INPUT DIGITAL CHANNELS
+            t = new Thread(Check_Digital_Inputs);
+            t.Start();
+            Console.WriteLine("Thread Iniciada");
+            
+        }
+
+        //Luzes de nevoeiro, médio e maximos (dependente da luz), luz matricula, luz interior,
+        public void Check_Digital_Inputs()
+        {
+
+            while (true)
+            {
+
+                //Sensor Noite
+                if (K8055.ReadDigitalChannel(1) && aux == 0)
+                {
+                    aux = 1;
+                    
+                    // Ligar luzes da matricula
+                    K8055.SetDigitalChannel(1);
+                    
+                    // Se nenhuma luz estiver ligada, ligamos os medios
+                    if(ligado == -1)
+                    {
+                        K8055.SetDigitalChannel(7);
+                    }
+
+                }
+                else if (!K8055.ReadDigitalChannel(1) && aux == 1)
+                {
+                    aux = 0;
+                    if(ligado == -1)
+                    {
+                        K8055.ClearDigitalChannel(7);
+                        K8055.ClearDigitalChannel(8);
+                    }
+                    K8055.ClearDigitalChannel(1);
+                }
+
+                //Sensor Nevoeiro
+                if (K8055.ReadDigitalChannel(2))
+                {
+                    aux2 = 0;
+                    K8055.SetDigitalChannel(6);
+
+                }
+                else if (!K8055.ReadDigitalChannel(2) && aux2 == 0)
+                {
+                    aux2 = 1;
+                    K8055.ClearDigitalChannel(6);
+
+                }
+
+                //Sensor Portas
+                if (K8055.ReadDigitalChannel(3))
+                {
+                    K8055.SetDigitalChannel(2);
+
+                }
+                else
+                {
+                    K8055.ClearDigitalChannel(2);
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            board_open = false;
+            K8055.ClearDigitalChannel(1);
+            K8055.ClearDigitalChannel(2);
+            K8055.ClearDigitalChannel(3);
+            K8055.ClearDigitalChannel(4);
+            K8055.ClearDigitalChannel(5);
+            K8055.ClearDigitalChannel(6);
+            K8055.ClearDigitalChannel(7);
+            K8055.ClearDigitalChannel(8);
+            Luzes.Text = "Luzes Desligadas";
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            K8055.CloseDevice(); //Closes communication with the K8055
+            
+
+        }
+
+        //Mínimos, médios e máximos
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+                if (Luzes.SelectedItem.Equals("Luzes Desligadas"))
+                {
+                    ligado = -1;
+                    K8055.ClearDigitalChannel(7);
+                    K8055.ClearDigitalChannel(8);
+                }
+
+                if (Luzes.SelectedItem.Equals("Mínimos"))
+                {
+                    ligado = 1;
+                    K8055.ClearDigitalChannel(7);
+                    K8055.SetDigitalChannel(8);
+                }
+
+                if (Luzes.SelectedItem.Equals("Médios"))
+                {
+                    ligado = 1;
+                    K8055.ClearDigitalChannel(8);
+                    K8055.SetDigitalChannel(7);
+                }
+
+                if (Luzes.SelectedItem.Equals("Máximos"))
+                {
+                    ligado = 1;
+                    K8055.SetDigitalChannel(8);
+                    K8055.SetDigitalChannel(7);
+                }
+            
+
+
+        }
+
+
+        //Luzes de Nevoeiro
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        { 
+        
+            
+            if (checkBox1.Checked && !K8055.ReadDigitalChannel(2))
+            {
+                K8055.SetDigitalChannel(6);
+            }
+            
+            else if (!checkBox1.Checked && !K8055.ReadDigitalChannel(2))
+            {
+                K8055.ClearDigitalChannel(6);
+            }
+        }
+
+        //Travão
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                K8055.SetDigitalChannel(4);
+            }
+            else
+            {
+                K8055.ClearDigitalChannel(4);
+            }
+        }
+
+        //Marcha atrás
+        private async void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                while (checkBox3.Checked)
+                {
+
+                    K8055.SetDigitalChannel(5);
+
+                    await Task.Delay(2000);
+                    //Console.WriteLine("stop wait timer");
+
+                    K8055.ClearDigitalChannel(5);
+
+                    await Task.Delay(2000);
+                    //Console.WriteLine("stop wait timer");
+                }
+            }
+            else
+            {
+                K8055.ClearDigitalChannel(5);
+            }
+        }
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                K8055.SetDigitalChannel(3);
+
+            }
+            else
+            {
+                K8055.ClearDigitalChannel(3);
+
+            }        
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
